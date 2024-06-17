84c65897281b9874a79383686035ef427687df72
varun <varunjohn@gmail.com>
2024-06-16 20:16:34 -0400

    Add project files.

---
+++ TestExam/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestExam
+{
+    internal class Product
+    {
+        public int Id { get; set; }
+
+        private string _name = "";
+
+        public string Name { 
+            get { return _name;}
+            set { 
+                if(value != null) {
+                    _name = value;
+                }
+            }
+        }
+
+        public string Description { get; set; }
+
+    }
+}
---
+++ TestExam/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using TestExam;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Product product = new Product();
+        product.Name
+        Console.WriteLine("Mid Term");
+    }
+}
